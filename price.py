6c205817c0543c32a8600b47dbd4d4b9af7c91e4
shuja2003 <shuja.nining@gmail.com>
2026-07-24 23:21:40 +0530

    Update price.py

--- price.py
+++ price.py
@@ -1,25 +1,9 @@
-import os
-import asyncio
-from telegram import Bot
-from price import get_btc_price
+import requests
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+def get_btc_price():
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 
-bot = Bot(token=BOT_TOKEN)
+    response = requests.get(url, timeout=10)
+    data = response.json()
 
-async def main():
-    btc_price = get_btc_price()
-
-    message = (
-        "₿ Shuja BTC 5M Monitor\n\n"
-        f"BTC Price: ${btc_price:,.2f}\n\n"
-        "Status: Watching 5-minute market..."
-    )
-
-    await bot.send_message(
-        chat_id=CHAT_ID,
-        text=message
-    )
-
-asyncio.run(main())
+    return float(data["price"])
