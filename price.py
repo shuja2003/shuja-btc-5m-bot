fb536f74c2b9a525a74fbeea3d4637e77de5292f
shuja2003 <shuja.nining@gmail.com>
2026-07-24 23:32:35 +0530

    Update price.py

--- price.py
+++ price.py
@@ -6,4 +6,7 @@ def get_btc_price():
     response = requests.get(url, timeout=10)
     data = response.json()
 
+    if "price" not in data:
+        raise Exception(f"Binance error: {data}")
+
     return float(data["price"])
