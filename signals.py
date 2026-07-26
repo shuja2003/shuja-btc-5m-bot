845bf2d3d8d0f5688951d6eb9c9d3ff560e7d66b
shuja2003 <shuja.nining@gmail.com>
2026-07-26 23:01:04 +0530

    Update signals.py

--- signals.py
+++ signals.py
@@ -1,11 +1,11 @@
 def get_signal(start_price, current_price):
     change = ((current_price - start_price) / start_price) * 100
 
-    if change > 0.05:
-        signal = "BUY 🟢"
-    elif change < -0.05:
-        signal = "SELL 🔴"
+    if change >= 0.02:
+        signal = "🟢 BUY"
+    elif change <= -0.02:
+        signal = "🔴 SELL"
     else:
-        signal = "WAIT ⏳"
+        signal = "⏳ WAIT"
 
     return signal, change
