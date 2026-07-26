01c6a64cefa689d7a2c1a5e47d58e557ee80b4c1
shuja2003 <shuja.nining@gmail.com>
2026-07-26 22:52:23 +0530

    Update price.py

--- price.py
+++ price.py
@@ -1,11 +1,9 @@
 import requests
 
 def get_btc_price():
-    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+    url = "https://data-api.binance.vision/api/v3/ticker/price?symbol=BTCUSDT"
 
     response = requests.get(url, timeout=10)
-    response.raise_for_status()
-
     data = response.json()
 
     return float(data["price"])
