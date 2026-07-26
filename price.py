cc11bcbd998190fd6e6673aa10ae37abebbf0be5
shuja2003 <shuja.nining@gmail.com>
2026-07-26 22:48:07 +0530

    Update price.py

--- price.py
+++ price.py
@@ -1,9 +1,11 @@
 import requests
 
 def get_btc_price():
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 
     response = requests.get(url, timeout=10)
+    response.raise_for_status()
+
     data = response.json()
 
-    return float(data["bitcoin"]["usd"])
+    return float(data["price"])
