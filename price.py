fd9a4f8729718fdef9b6a21ff41efadf7c876efc
shuja2003 <shuja.nining@gmail.com>
2026-07-28 18:09:26 +0530

    Update price.py

--- price.py
+++ price.py
@@ -1,4 +1,6 @@
 import requests
+import time
+
 
 def get_btc_price():
     url = "https://data-api.binance.vision/api/v3/ticker/price?symbol=BTCUSDT"
@@ -7,3 +9,14 @@ def get_btc_price():
     data = response.json()
 
     return float(data["price"])
+
+
+def get_candle_open_price():
+    url = "https://data-api.binance.vision/api/v3/klines?symbol=BTCUSDT&interval=5m&limit=1"
+
+    response = requests.get(url, timeout=10)
+    data = response.json()
+
+    candle_open = float(data[0][1])
+
+    return candle_open
