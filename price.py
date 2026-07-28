23ca3b3a3e4d7b418788a4088afb0aca14dccf1f
shuja2003 <shuja.nining@gmail.com>
2026-07-28 14:52:21 +0530

    Update price.py

--- price.py
+++ price.py
@@ -1,12 +1,9 @@
 import requests
 
-def get_klines(limit=30):
-    url = (
-        f"https://data-api.binance.vision/api/v3/klines"
-        f"?symbol=BTCUSDT&interval=5m&limit={limit}"
-    )
+def get_btc_price():
+    url = "https://data-api.binance.vision/api/v3/ticker/price?symbol=BTCUSDT"
 
     response = requests.get(url, timeout=10)
-    response.raise_for_status()
+    data = response.json()
 
-    return response.json()
+    return float(data["price"])
