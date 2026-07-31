9044f31733d942f96fd9cb155e4b45fdbd3f5af0
shuja2003 <shuja.nining@gmail.com>
2026-07-31 10:28:25 +0530

    Update price.py

--- price.py
+++ price.py
@@ -26,3 +26,29 @@ def get_binance_time():
     data = response.json()
 
     return int(data["serverTime"] / 1000)
+
+
+def get_trend():
+    url = "https://data-api.binance.vision/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=5"
+
+    response = requests.get(url, timeout=10)
+    data = response.json()
+
+    up = 0
+    down = 0
+
+    for candle in data:
+        open_price = float(candle[1])
+        close_price = float(candle[4])
+
+        if close_price > open_price:
+            up += 1
+        elif close_price < open_price:
+            down += 1
+
+    if up >= 3:
+        return "UP"
+    elif down >= 3:
+        return "DOWN"
+    else:
+        return "SIDEWAYS"
