723433e6ffdc58c633951dbed268f23529960a75
shuja2003 <shuja.nining@gmail.com>
2026-08-01 17:27:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,10 +19,12 @@ async def run_bot():
             print(f"Seconds left: {seconds_left}", flush=True)
 
             checkpoints = [120, 60, 30]
-            previous = seconds_left
 
-            for checkpoint in checkpoints:
-                wait_time = previous - checkpoint
+for checkpoint in checkpoints:
+    if seconds_left > checkpoint:
+        wait_time = seconds_left - checkpoint
+    else:
+        continue
 
                 if wait_time > 0:
                     print(f"Waiting {wait_time} seconds...", flush=True)
